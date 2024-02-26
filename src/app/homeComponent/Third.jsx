cc6ccb0c4f320915c480b886351816e12c01761b
meadarsh <itsadarsh33@gmail.com>
2024-02-26 21:14:53 +0530

    mt

--- src/app/homeComponent/Third.jsx
+++ src/app/homeComponent/Third.jsx
@@ -4,7 +4,7 @@ import { buttons } from '../Data/Homepage'
 const ThirdPage = () => {
   return (
     <div className='min-h-[20vh] w-full border-t border-gray-400  justify-evenly items-center flex-wrap flex mt-[15vh]'>
-    {buttons.map((data,index)=>(<div key={index} className="button cursor-pointer h-12 border lg:hover:bg-black lg:dark:hover:bg-white lg:hover:text-white lg:dark:hover:text-black border-black dark:border-white px-5 flex items-center text-xl rounded-full ">
+    {buttons.map((data,index)=>(<div key={index} className="button lg:mt-0 mt-3 cursor-pointer h-12 border lg:hover:bg-black lg:dark:hover:bg-white lg:hover:text-white lg:dark:hover:text-black border-black dark:border-white px-5 flex items-center text-xl rounded-full ">
         <p>{data}</p>
      </div>)) }
     </div>
